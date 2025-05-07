d3f1f007a4ab3087042e3ce10d2c725468399c3e
Maurício de Oliveira e Silva <Mauricio50500546@edu.df.senac.br>
2025-05-07 17:27:19 -0300

    Pulo quase pronto

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Scripting.APIUpdating;
 
 public class Player : MonoBehaviour
@@ -9,6 +10,7 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb2d;
     public float jumpForce;
     [SerializeField] private bool onGround;
+    private bool wasonGround;
     private bool isJump;
     private Collider2D[] collider_1, collider_2;
     private float checkRadius = 0.1f;
@@ -32,6 +34,20 @@ private void Move()
 {
     rb2d.linearVelocity = new UnityEngine.Vector2 (moveInput * moveSpeed, rb2d.linearVelocity.y);
 }
+void checkGround()
+{
+    collider_1 = Physics2D.OverlapCircleAll(groundCheck[0].position, checkRadius, groundMask);
+    collider_2 = Physics2D.OverlapCircleAll(groundCheck[1].position, checkRadius, groundMask);
+
+    if (collider_1.Length>0 || collider_2.Length>0)
+    {
+        onGround = true;
+    }
+    else
+    {
+        onGround = false;
+    }
+}
 private void InputSistem()
 {
     moveInput = Input.GetAxisRaw("Horizontal");
@@ -39,5 +55,13 @@ private void InputSistem()
     {
         transform.localScale = new UnityEngine.Vector3(moveInput,  1f, 1f);
     }
+    if(Input.GetKeyDown(KeyCode.Space) && onGround)
+    {
+        Jump();
+    }
+}
+private void Jump()
+{
+    rb2d.linearVelocity = new UnityEngine.Vector2 (rb2d.linearVelocity.x,jumpForce);
 }
 }
